b04129b96b0b66781c6950b109e0b8751d5efee6
Cyrus Gabilla <cgabilla@gmail.com>
2018-06-09 17:09:09 +0800

    Add pyramid project setup

---
+++ i_asiv_backend_pyramid/i_asiv_backend_pyramid/__init__.py
@@ -0,0 +1,17 @@
+from pyramid.config import Configurator
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+
+    ##
+    config.add_route('cities', '/cities')
+    config.add_route('city', '/cities/{name}')
+
+    config.scan()
+    return config.make_wsgi_app()
